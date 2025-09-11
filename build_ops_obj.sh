e6b94da27024c58b2c5067293acf866b44f17980
Nicolas Kruse <nicolas@nonan.net>
2025-09-11 22:47:30 +0200

    optimizations added

--- build_ops_obj.sh
+++ build_ops_obj.sh
@@ -4,4 +4,7 @@ set -v
 python src/copapy/generate_stencils.py
 mkdir -p src/copapy/obj
 gcc -c src/copapy/stencils.c -o src/copapy/obj/stencils_x86_64.o
+gcc -c src/copapy/stencils.c -O1 -o src/copapy/obj/stencils_x86_64_O1.o
+gcc -c src/copapy/stencils.c -O2 -o src/copapy/obj/stencils_x86_64_O2.o
+gcc -c src/copapy/stencils.c -O3 -o src/copapy/obj/stencils_x86_64_O3.o
 ls -l src/copapy/obj/*.o
