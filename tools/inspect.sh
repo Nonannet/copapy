963976fe90f3f5c8f6c3b336e3cecf40e00049c9
Nicolas <Nicolas@nonan.net>
2025-10-14 23:06:34 +0200

    changed python to python3 in bash scripts

--- tools/inspect.sh
+++ tools/inspect.sh
@@ -2,6 +2,6 @@
 
 source tools/build.sh
 objdump -d -x src/copapy/obj/stencils_x86_64_O3.o > bin/stencils_x86_64_O3.asm
-python tools/make_example.py
-python tools/extract_code.py "bin/test.copapy" "bin/test.copapy.bin"
+python3 tools/make_example.py
+python3 tools/extract_code.py "bin/test.copapy" "bin/test.copapy.bin"
 objdump -D -b binary -m i386:x86-64 --adjust-vma=0x1000 bin/test.copapy.bin > bin/test.copapy.asm
