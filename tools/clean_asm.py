39b803c17daa6b6942d27147496f815c6d3e90ba
Nicolas <Nicolas@nonan.net>
2025-10-15 23:15:47 +0200

    ci: coloring fixed

--- tools/clean_asm.py
+++ tools/clean_asm.py
@@ -1,29 +1,31 @@
 import argparse
 import re
 
-def main():
+def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("path", type=str, help="Input file path")
-    #parser.add_argument("--abi", type=str, default="", help="Optionaler String (Standard: '')")
     args = parser.parse_args()
 
     regex = r"(\:\n)(.*?)(^[^\n]+\n[^\n]+result_.*?\n\n)"
-    subst = "\\g<1><b>\\g<2></b>\\g<3>"
+    subst = "\\g<1><b>\\g<2></b><span style=\"color:grey\">\\g<3></span>"
 
     with open(args.path, 'rt') as f:
         text = f.read()
 
     outp_flag = "Disassembly of section .text" not in text
 
+    text = text.replace('\t', ' ' * 4)
     text = text.replace('>', '&gt;')
     text = text.replace('<', '&lt;')
 
     text = re.sub(regex, subst, text, 0, re.MULTILINE | re.DOTALL)
 
+    text = re.sub(r"  +", "&nbsp;", text, 0, re.MULTILINE | re.DOTALL)
+
     print('<code>')
     for line in text.splitlines():
         if outp_flag:
-            print(line.replace(' ', '&nbsp;') + '<br>')
+            print(line + '<br>')
 
         if "Disassembly of section .text:" in line:
             outp_flag = True
