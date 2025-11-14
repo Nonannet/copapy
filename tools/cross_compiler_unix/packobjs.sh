270514c2dbda5df8942c5457fc12cfe70b1cf970
Nicolas <Nicolas@nonan.net>
2025-11-14 10:08:43 +0100

    -ffunction-sections added for stencil compilation and -fno-pic removed for musl compilation

--- tools/cross_compiler_unix/packobjs.sh
+++ tools/cross_compiler_unix/packobjs.sh
@@ -8,7 +8,7 @@ LD_NAME=$2
 DEST_FILE=$3
 
 echo "- Config musl"
-./configure CFLAGS="-O2 -fno-pic -fno-stack-protector" CC=$CC_NAME
+./configure CFLAGS="-O2 -fno-stack-protector" CC=$CC_NAME
 
 echo "- Build musl"
 make clean
@@ -18,15 +18,15 @@ mkdir -p ../build/stencil_objs
 
 echo "- Extracting required objects"
 cd ../build/stencil_objs
-ar x ../../musl/lib/libc.a sinf.o cosf.o tanf.o asinf.o acosf.o atanf.o atan2f.o
-ar x ../../musl/lib/libc.a sqrtf.o logf.o expf.o sqrt.o
-ar x ../../musl/lib/libc.a logf_data.o __tandf.o __cosdf.o __sindf.o
-ar x ../../musl/lib/libc.a fabsf.o scalbn.o floor.o exp2f_data.o powf.o powf_data.o
-ar x ../../musl/lib/libc.a __rem_pio2f.o __math_invalidf.o __stack_chk_fail.o __math_divzerof.o __math_oflowf.o __rem_pio2_large.o __math_uflowf.o __math_xflowf.o
+ar x ../../musl/lib/libc.a sinf.lo cosf.lo tanf.lo asinf.lo acosf.lo atanf.lo atan2f.lo
+ar x ../../musl/lib/libc.a sqrtf.lo logf.lo expf.lo sqrt.lo
+ar x ../../musl/lib/libc.a logf_data.lo __tandf.lo __cosdf.lo __sindf.lo
+ar x ../../musl/lib/libc.a fabsf.lo scalbn.lo floor.lo exp2f_data.lo powf.lo powf_data.lo
+ar x ../../musl/lib/libc.a __rem_pio2f.lo __math_invalidf.lo __stack_chk_fail.lo __math_divzerof.lo __math_oflowf.lo __rem_pio2_large.lo __math_uflowf.lo __math_xflowf.lo
 
 cd ../../musl
 
 echo "- Merge objects"
-$LD_NAME -r ../build/stencil_objs/*.o -o $DEST_FILE 
+$LD_NAME -r ../build/stencil_objs/*.lo -o $DEST_FILE 
 
 rm ../build/stencil_objs/*
