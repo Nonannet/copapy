b653b734db32702b76d2afd493a6fb5a863485f1
Nicolas <Nicolas@nonan.net>
2025-11-23 17:10:53 +0100

    stencil alignment for ARM set to 4 - gcc complaints otherwise

--- stencils/stencil_helper.h
+++ stencils/stencil_helper.h
@@ -3,7 +3,11 @@
 // Remove function alignment for stencils
 #if defined(__GNUC__)
 #define NOINLINE __attribute__((noinline))
+#if defined(__aarch64__) || defined(_M_ARM64) || defined(__arm__) || defined(__thumb__) || defined(_M_ARM)
+#define STENCIL __attribute__((aligned(4)))
+#else
 #define STENCIL __attribute__((aligned(1)))
+#endif
 #else
 #define NOINLINE
 #define STENCIL
