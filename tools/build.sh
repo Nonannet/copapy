df5b4c19f1944e67f13bff05bdcc61da986cc8e4
Nicolas <Nicolas@nonan.net>
2026-01-01 15:20:09 +0100

    manual stencil build scripts updated with cl arguments and x86_64 default

--- tools/build.sh
+++ tools/build.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-set -e
-set -v
+set -eux
+
+ARCH=${1:-x86_64}
+
+case "$ARCH" in
+    (x86_64|arm-v6|arm-v7|all)
+        ;;
+    (*)
+        echo "Usage: $0 [x86_64|arm-v6|arm-v7|all]"
+        exit 1
+        ;;
+esac
 
 mkdir -p build/stencils
 mkdir -p build/runner
@@ -10,34 +20,90 @@ DEST=src/copapy/obj
 python3 stencils/generate_stencils.py $SRC
 mkdir -p $DEST
 
-gcc -fno-pic -ffunction-sections -c $SRC -O3 -o build/stencils/stencils.o
-ld -r build/stencils/stencils.o build/musl/musl_objects_x86_64.o -o $DEST/stencils_x86_64_O3.o
-objdump -d -x $DEST/stencils_x86_64_O3.o > build/stencils/stencils_x86_64_O3.asm
+#######################################
+# x86_64
+#######################################
+if [[ "$ARCH" == "x86_64" || "$ARCH" == "all" ]]; then
+    echo "--------------x86_64----------------"
 
-mkdir bin -p
-gcc -Wall -Wextra -Wconversion -Wsign-conversion \
-    -Wshadow -Wstrict-overflow -Werror -g -O3 \
-    -DENABLE_LOGGING \
-    src/coparun/runmem.c src/coparun/coparun.c src/coparun/mem_man.c -o build/runner/coparun
+    gcc -fno-pic -ffunction-sections -c $SRC -O3 -o build/stencils/stencils.o
+    ld -r build/stencils/stencils.o build/musl/musl_objects_x86_64.o \
+        -o $DEST/stencils_x86_64_O3.o
+    objdump -d -x $DEST/stencils_x86_64_O3.o \
+        > build/stencils/stencils_x86_64_O3.asm
 
+    mkdir -p bin
+    gcc -Wall -Wextra -Wconversion -Wsign-conversion \
+        -Wshadow -Wstrict-overflow -Werror -g -O3 \
+        -DENABLE_LOGGING \
+        src/coparun/runmem.c \
+        src/coparun/coparun.c \
+        src/coparun/mem_man.c \
+        -o build/runner/coparun
+fi
 
-echo "--------------arm-v6  32 bit----------------"
-LIBGCC=$(arm-none-eabi-gcc -print-libgcc-file-name)
-#LIBM=$(arm-none-eabi-gcc -print-file-name=libm.a)
-#LIBC=$(arm-none-eabi-gcc -print-file-name=libc.a)
+#######################################
+# ARM v6
+#######################################
+if [[ "$ARCH" == "arm-v6" || "$ARCH" == "all" ]]; then
+    echo "--------------arm-v6 32 bit----------------"
 
-arm-none-eabi-gcc -fno-pic -ffunction-sections -march=armv6 -mfpu=vfp -mfloat-abi=hard -marm -c $SRC -O3 -o build/stencils/stencils.o
-arm-none-eabi-ld -r build/stencils/stencils.o build/musl/musl_objects_armv6.o $LIBGCC -o $DEST/stencils_armv6_O3.o
-arm-none-eabi-objdump -d -x $DEST/stencils_armv6_O3.o > build/stencils/stencils_armv6_O3.asm
-arm-linux-gnueabihf-gcc -march=armv6 -mfpu=vfp -mfloat-abi=hard -marm -static -Wall -Wextra -Wconversion -Wsign-conversion -Wshadow -Wstrict-overflow -O3 -DENABLE_LOGGING src/coparun/runmem.c src/coparun/coparun.c src/coparun/mem_man.c -o build/runner/coparun-armv6
+    LIBGCC=$(arm-none-eabi-gcc -print-libgcc-file-name)
 
+    arm-none-eabi-gcc -fno-pic -ffunction-sections \
+        -march=armv6 -mfpu=vfp -mfloat-abi=hard -marm \
+        -c $SRC -O3 -o build/stencils/stencils.o
 
-echo "--------------arm-v7  32 bit----------------"
-LIBGCC=$(arm-none-eabi-gcc -print-libgcc-file-name)
-#LIBM=$(arm-none-eabi-gcc -print-file-name=libm.a)
-#LIBC=$(arm-none-eabi-gcc -print-file-name=libc.a)
+    arm-none-eabi-ld -r \
+        build/stencils/stencils.o \
+        build/musl/musl_objects_armv6.o \
+        $LIBGCC \
+        -o $DEST/stencils_armv6_O3.o
 
-arm-none-eabi-gcc -fno-pic -ffunction-sections -march=armv7-a -mfpu=neon-vfpv3 -mfloat-abi=hard -marm -c $SRC -O3 -o build/stencils/stencils.o
-arm-none-eabi-ld -r build/stencils/stencils.o build/musl/musl_objects_armv7.o $LIBGCC -o $DEST/stencils_armv7_O3.o
-arm-none-eabi-objdump -d -x $DEST/stencils_armv7_O3.o > build/stencils/stencils_armv7_O3.asm
-arm-linux-gnueabihf-gcc -march=armv7-a -mfpu=neon-vfpv3 -mfloat-abi=hard -marm -static -Wall -Wextra -Wconversion -Wsign-conversion -Wshadow -Wstrict-overflow -O3 -DENABLE_LOGGING src/coparun/runmem.c src/coparun/coparun.c src/coparun/mem_man.c -o build/runner/coparun-armv7
+    arm-none-eabi-objdump -d -x \
+        $DEST/stencils_armv6_O3.o \
+        > build/stencils/stencils_armv6_O3.asm
+
+    arm-linux-gnueabihf-gcc \
+        -march=armv6 -mfpu=vfp -mfloat-abi=hard -marm -static \
+        -Wall -Wextra -Wconversion -Wsign-conversion \
+        -Wshadow -Wstrict-overflow -O3 \
+        -DENABLE_LOGGING \
+        src/coparun/runmem.c \
+        src/coparun/coparun.c \
+        src/coparun/mem_man.c \
+        -o build/runner/coparun-armv6
+fi
+
+#######################################
+# ARM v7
+#######################################
+if [[ "$ARCH" == "arm-v7" || "$ARCH" == "all" ]]; then
+    echo "--------------arm-v7 32 bit----------------"
+
+    LIBGCC=$(arm-none-eabi-gcc -print-libgcc-file-name)
+
+    arm-none-eabi-gcc -fno-pic -ffunction-sections \
+        -march=armv7-a -mfpu=neon-vfpv3 -mfloat-abi=hard -marm \
+        -c $SRC -O3 -o build/stencils/stencils.o
+
+    arm-none-eabi-ld -r \
+        build/stencils/stencils.o \
+        build/musl/musl_objects_armv7.o \
+        $LIBGCC \
+        -o $DEST/stencils_armv7_O3.o
+
+    arm-none-eabi-objdump -d -x \
+        $DEST/stencils_armv7_O3.o \
+        > build/stencils/stencils_armv7_O3.asm
+
+    arm-linux-gnueabihf-gcc \
+        -march=armv7-a -mfpu=neon-vfpv3 -mfloat-abi=hard -marm -static \
+        -Wall -Wextra -Wconversion -Wsign-conversion \
+        -Wshadow -Wstrict-overflow -O3 \
+        -DENABLE_LOGGING \
+        src/coparun/runmem.c \
+        src/coparun/coparun.c \
+        src/coparun/mem_man.c \
+        -o build/runner/coparun-armv7
+fi
