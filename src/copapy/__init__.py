09d7d01eeead4880261e19dd7cdb22eaf37e332b
Nicolas Kruse <nicolas@nonan.net>
2026-03-27 15:22:33 +0100

    quaternion class added

--- src/copapy/__init__.py
+++ src/copapy/__init__.py
@@ -36,6 +36,7 @@ Example usage:
 from ._target import Target, jit
 from ._basic_types import NumLike, value, generic_sdb, iif
 from ._vectors import vector, distance, scalar_projection, angle_between, rotate_vector, vector_projection
+from ._quaternion import quaternion
 from ._tensors import tensor, zeros, ones, arange, eye, identity, diagonal
 from ._math import sqrt, abs, sign, sin, cos, tan, asin, acos, atan, atan2, log, exp, pow, get_42, clamp, min, max, relu
 from ._autograd import grad
@@ -80,7 +81,8 @@ __all__ = [
     "scalar_projection",
     "angle_between",
     "rotate_vector",
-    "vector_projection",
+"vector_projection",
+    "quaternion",
     "grad",
     "eye",
     "jit"
