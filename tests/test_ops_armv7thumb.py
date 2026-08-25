4bb1bb56b610ac609cbcc241139807281ed7b66a
Nicolas <Nicolas@nonan.net>
2026-08-25 13:39:19 +0200

    Tests updated to skip armv7thumb test on WSL since ARM qemu seams not working on WSL1

--- tests/test_ops_armv7thumb.py
+++ tests/test_ops_armv7thumb.py
@@ -129,7 +129,10 @@ def test_compile():
     if not check_for_qemu():
         warnings.warn("qemu-armv7 not found, armv7 test skipped!", UserWarning)
         return
-    if not os.path.isfile('build/runner/coparun-armv7'):
+    if "wsl" in qemu_command:
+        warnings.warn("qemu-armv7 seams not work on wsl1, test skipped!", UserWarning)
+        return
+    if not os.path.isfile("build/runner/coparun-armv7"):
         warnings.warn("armv7 runner not found, armv7 test skipped!", UserWarning)
         return
 
