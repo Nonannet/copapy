9e941c1a08253327270a0826760cc6be05b32ab8
Nicolas <Nicolas@nonan.net>
2026-09-21 17:56:10 +0200

    CI: Updated windows build gh action to handle the asm-file

--- tools/build.bat
+++ tools/build.bat
@@ -37,17 +37,13 @@ cl /Zi /Od stencils\test.c /Fe:build\stencils\test.exe
 echo - Build entry wrapper...
 ml64 /c /Fobuild\runner\x86_64_abi_shim.obj src\coparun\x86_64_abi_shim.asm
 
-echo - Compile runner C files...
-cl /Zi /O2 /DENABLE_LOGGING /c src\coparun\runmem.c /Fobuild\runner\runmem.obj
-cl /Zi /O2 /DENABLE_LOGGING /c src\coparun\coparun.c /Fobuild\runner\coparun.obj
-cl /Zi /O2 /DENABLE_LOGGING /c src\coparun\mem_man.c /Fobuild\runner\mem_man.obj
-
-echo - Link runner...
-link /OUT:build\runner\coparun.exe ^
-    build\runner\runmem.obj ^
-    build\runner\coparun.obj ^
-    build\runner\mem_man.obj ^
-    build\runner\x86_64_abi_shim.obj
+echo - Compile and link runner...
+cl /Zi /O2 /DENABLE_LOGGING /Fobuild\runner\ ^
+    src\coparun\runmem.c ^
+    src\coparun\coparun.c ^
+    src\coparun\mem_man.c ^
+    build\runner\x86_64_abi_shim.obj ^
+    /Fe:build\runner\coparun.exe
 
 echo - Build stencils for x86_64...
 wsl gcc -fno-pic -ffunction-sections -c build/stencils/stencils.c -O3 -o build/stencils/stencils.o
